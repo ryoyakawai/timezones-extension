135f61792e88f2693385eaaebe2b1afa0af70226
ryoyakawai <ryoya.kawai@gmail.com>
2018-06-20 12:42:01 -0700

    update

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,14 +1,14 @@
 (() => {
     let injectFiles = [
-        'scripts/timezonedef.js',
-        'scripts/timezoneclock.js'
+        { type: 'moule', path: 'scripts/timezonedef.js' },
+        { type: 'module', path: 'scripts/timezoneclock.js' }
     ];
 
     for(let i in injectFiles) {
-        const src = chrome.extension.getURL(injectFiles[i]);
+        const src = chrome.extension.getURL(injectFiles[i].path);
         const script = document.createElement('script');
         script.setAttribute('src', src);
-        script.setAttribute('type', 'module');
+        script.setAttribute('type', injectFiles[i].type);
         document.body.appendChild(script);
     }
 })();
